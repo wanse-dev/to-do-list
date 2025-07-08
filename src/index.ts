45b00e0ddd6f4aeee6e0dbdcea2b88bb65101ed6
wanse-dev <wansedev@gmail.com>
2025-07-08 01:32:41 -0300

    added connection to server using express

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import routes from "./routes/index";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
